d5c3061a86bc3056e70c2358ac932d1e6c93aed8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-15 17:00:33 +0530

    feat: Added model GithubProject

---
+++ src/main/java/io/yadnyesh/zerotohero/model/GithubProject.java
@@ -0,0 +1,33 @@
+package io.yadnyesh.zerotohero.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class GithubProject implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String orgName;
+	
+	@Column(unique = true)
+	private String repoName;
+	
+	public GithubProject() {
+	}
+	
+	public GithubProject(String orgName, String repoName) {
+		this.orgName = orgName;
+		this.repoName = repoName;
+	}
+	
+	@Override
+	public String toString() {
+		return "GithubProject{" + "id=" + id + ", orgName='" + orgName + '\'' + ", repoName='" + repoName + '\'' + '}';
+	}
+}
