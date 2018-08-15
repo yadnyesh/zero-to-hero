84585acb0aaadc6f549e8a60bbe7184c9585104c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-15 16:43:51 +0530

    First Commit

---
+++ src/main/java/io/yadnyesh/zerotohero/ZeroToHeroApplication.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.zerotohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroToHeroApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroToHeroApplication.class, args);
+	}
+}
---
+++ src/test/java/io/yadnyesh/zerotohero/ZeroToHeroApplicationTests.java
@@ -0,0 +1,16 @@
+package io.yadnyesh.zerotohero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZeroToHeroApplicationTests {
+	
+	@Test
+	public void contextLoads() {
+	}
+	
+}
