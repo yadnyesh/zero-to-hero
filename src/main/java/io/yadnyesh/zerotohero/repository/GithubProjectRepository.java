c90009737c5f24098b9600170de681815e47e161
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-15 17:05:18 +0530

    feat: Added  GithubProjectRepository

--- src/main/java/io/yadnyesh/zerotohero/repository/GithubProjectRepository.java
+++ src/main/java/io/yadnyesh/zerotohero/repository/GithubProjectRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface GithubProjectRepository extends PagingAndSortingRepository<GithubProject, Long> {
 
+	GithubProject findByRepoName(String repoName);
 }
